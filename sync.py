8ce2ce6f04ae7b0c01ffa7e76708292d6ddec85f
Weili An <China_Aisa@live.com>
2017-09-29 12:22:24 +0800

    Add GroupChat files support

--- sync.py
+++ sync.py
@@ -5,7 +5,7 @@ from itchat.content import *
 
 integromatHook = ''
 
-@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO],isFriendChat=True,isGroupChat=True)
 def download_files(msg):
     # IPython.embed()
     url, params, headers, core = msg.download(msg.fileName)
@@ -15,5 +15,5 @@ def download_files(msg):
                         data={"filename":msg.fileName,"data":base64.encodebytes(file_res.content).decode()})
     itchat.send("Tried to upload: %s; Status: %s; Download_link: %s" % (msg.fileName, res.status_code, res.text if res.status_code==200 else ''), toUserName='filehelper')
 
-itchat.auto_login(enableCmdQR=True,hotReload=True)
+itchat.auto_login(enableCmdQR=True,hotReload=False)
 itchat.run(True)
